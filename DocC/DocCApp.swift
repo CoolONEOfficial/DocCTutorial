d125c53156472a20490c37cc33f4b12e15322d7c
Nikolai Trukhin <NTrukhin@alfabank.ru>
2023-08-25 21:44:03 +0200

    Initial Commit

---
+++ DocC/DocCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocCApp.swift
+//  DocC
+//
+//  Created by Nikolai Trukhin on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct DocCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
