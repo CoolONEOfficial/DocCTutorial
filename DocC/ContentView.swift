68cf0843b920a950f87fe27d04691f1beebd8a5d
Nikolai Trukhin <coolone.official@gmail.com>
2023-08-28 13:59:34 +0200

    Add documentation example

--- DocC/ContentView.swift
+++ DocC/ContentView.swift
@@ -7,7 +7,40 @@
 
 import SwiftUI
 
+///
+
+enum SomeEnum {}
+
+/**
+ View with some content
+
+ Some long explanation
+ */
 struct ContentView: View {
+    /// Some func
+    /// # Title
+    /// ## Subtitle
+    ///
+    /// - List first element
+    ///  - Test
+    ///
+    ///  ```swift
+    /// let first = 1
+    ///  ```
+    ///
+    ///  ``SomeEnum``
+    ///
+    ///  [link](https://gogole.com)
+    ///
+    /// - Parameters:
+    ///  - one: First parameter with number value
+    ///
+    /// - Returns:
+    ///     Some computed number
+    func fun(one: Int) -> Int {
+        0
+    }
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
